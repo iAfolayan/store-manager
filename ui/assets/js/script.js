40a732f3b12df4fd8899b800b125573586b5b610
Afolayan Isaiah <iafolayanibikunle@gmail.com>
2018-10-12 11:39:43 +0100

    Implement mobile menu and fix some bugs

--- ui/assets/js/script.js
+++ ui/assets/js/script.js
@@ -1,19 +1,28 @@
-const displayMenu = (usrClick) => {
-	if (usrClick.style.display === "none") {
-		usrClick.style.display = 'block'
-	} else {
-		usrClick.style.display = 'none'
+(function(){
+	document.getElementById('logo').addEventListener('click', () => { location.href ='../index.html'});
+
+
+
+const slideMobileMenu = () => {
+		document.querySelector('#menuDrop').classList.toggle("showMenu");
+	}
+
+window.onclick = (event) => {
+	if (!event.target.matches('.mobileMenu')) {
+		const dropdowns = document.querySelector('.menuDropContent');
+		for(const counter of dropdowns) {
+			const openMenu = dropdowns[counter];
+
+			if (openMenu.classList.contains('showMenu')) {
+				openMenu.classList.remove('showMenu');
+			}	
+		}
+		/*for (counter = 0; counter < dropdowns.length; counter++) {
+			
+		}*/
 	}
 }
 
-(function(){
-	/*const usrChild = document.querySelector('.usrChild');
-	document.getElementById('usrManagement').addEventListener("click", displayMenu(usrChild));
-*/
-	/* product menu */
-	/*const prdChild = document.querySelector('.prdChild');
-	document.getElementById('prdManagement').addEventListener("click", displayMenu(prdChild));
-*/
-	document.getElementById('logo').addEventListener('click', () => { location.href ='../index.html'})
-})();
+document.querySelector('.mobileMenu').addEventListener("click", slideMobileMenu);
 
+})();
