62bc4acc51dc8527b310cef020b26576da49cd60
Afolayan Isaiah <iafolayanibikunle@gmail.com>
2018-10-18 12:21:09 +0100

    Implement testing

--- app/app.js
+++ app/app.js
@@ -2,17 +2,29 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 
+import routes from './routes';
+
 const app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Welcome to Store Manager 1.0'
+app.use('/api/v1', routes);
+
+// Error Handling
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      msg: error.message,
+    },
   });
 });
 
---
+++ app/data/sales.js
@@ -0,0 +1,63 @@
+const salesRecords = [{
+  id: 1,
+  sellerId: 'SA005',
+  saleAttendantName: 'Afolayan Isaiah',
+  productId: 2,
+  name: 'wireless bluetooth',
+  quantity: 50,
+  price: 2500,
+  totalAmount: 75000,
+  category: 'phone',
+  date: '2018-03-29T13:34:00.000'
+},
+{
+  id: 2,
+  sellerId: 'SA005',
+  saleAttendantName: 'Afolayan Ibikunle',
+  productId: 4,
+  name: 'wireless bluetooth',
+  quantity: 50,
+  price: 2500,
+  totalAmount: 75000,
+  category: 'phone',
+  date: '2018-03-29T13:34:00.000'
+},
+{
+  id: 3,
+  sellerId: 'SA055',
+  saleAttendantName: 'Afolayan Isaiah',
+  productId: 5,
+  name: 'wireless bluetooth',
+  quantity: 50,
+  price: 2500,
+  totalAmount: 75000,
+  category: 'Bluetooth',
+  date: '2018-03-29T13:34:00.000'
+},
+{
+  id: 4,
+  sellerId: 'SA005',
+  saleAttendantName: 'Afolayan Isaiah',
+  productId: 22,
+  name: 'wireless bluetooth',
+  quantity: 50,
+  price: 500,
+  totalAmount: 75000,
+  category: 'phone',
+  date: '2018-03-29T13:34:00.000'
+},
+{
+  id: 5,
+  sellerId: 'SA0057',
+  saleAttendantName: 'Afolayan Ayomide',
+  productId: 22,
+  name: 'Lazy Man Hanger',
+  quantity: 50,
+  price: 500,
+  totalAmount: 75000,
+  category: 'phone Accessory',
+  date: '2018-03-29T13:34:00.000'
+}];
+
+
+export default salesRecords;
