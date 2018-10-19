d29ab4fa0ab77753afbf127bf0e3bf2589464e52
Afolayan Isaiah <iafolayanibikunle@gmail.com>
2018-10-19 12:49:15 +0100

    Implement users authentication

--- app/data/users.js
+++ app/data/users.js
@@ -1,37 +1,67 @@
 const UsersData = [{
   id: 1,
   staffId: 'SM001',
-  password: 'admin123',
-  firstname: 'iAfolayan',
-  role: 1
+  title: 'Miss',
+  firstname: 'Afolayan',
+  lastname: 'Isaiah',
+  emailAddress: 'iafolayanibikunle@gmail.com',
+  phoneNumber: '08032167911',
+  role: 1,
+  gender: 'Male',
+  passport: 'admin.png',
+  contactAddress: '41, Osholake street, Ebute-meta, Lagos'
 },
 {
   id: 2,
   staffId: 'SM002',
-  password: 'admin123',
-  firstname: 'Ibikunle',
-  role: 1
+  title: 'Mr',
+  firstname: 'Ayomide',
+  lastname: 'Festus',
+  emailAddress: 'iafe@gmail.com',
+  phoneNumber: '08037911',
+  role: 1,
+  gender: 'Female',
+  passport: 'admina.png',
+  contactAddress: '42, Osholake street, Ebute-meta, Lagos'
 },
 {
   id: 3,
-  staffId: 'SM050',
-  password: '123456',
-  firstname: 'Olumide',
-  role: 2
+  staffId: 'SM003',
+  title: 'Mrs.',
+  firstname: 'Ogungbemi',
+  lastname: 'Olajumoke',
+  emailAddress: 'igrace@gmail.com',
+  phoneNumber: '07032167911',
+  role: 2,
+  gender: 'Female',
+  passport: 'adminadmin.png',
+  contactAddress: '43, Osholake street, Ebute-meta, Lagos'
 },
 {
   id: 4,
-  staffId: 'SM051',
-  password: '123456',
-  firstname: 'Ajayi',
-  role: 2
+  staffId: 'SM004',
+  title: 'Mrs.',
+  firstname: 'Adegbemi',
+  lastname: 'Olajke',
+  emailAddress: 'gdbjdsje@gmail.com',
+  phoneNumber: '0703225911',
+  role: 2,
+  gender: 'Female',
+  passport: 'admimin.png',
+  contactAddress: '44, Osholake street, Ebute-meta, Lagos'
 },
 {
   id: 5,
-  staffId: 'SM052',
-  password: '123456',
-  firstname: 'Smith',
-  role: 2
+  staffId: 'SM005',
+  title: 'Bro.',
+  firstname: 'Adeola',
+  lastname: 'Collins',
+  emailAddress: 'iadeolacollins@gmail.com',
+  phoneNumber: '07032167918',
+  role: 2,
+  gender: 'Male',
+  passport: 'admdmin.png',
+  contactAddress: '45, Osholake street, Ebute-meta, Lagos'
 }];
 
 export default UsersData;
