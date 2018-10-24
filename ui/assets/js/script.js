da26ef92d3b44c2e1e034924f74647ef08da1e6e
Afolayan Isaiah <iafolayanibikunle@gmail.com>
2018-10-24 21:24:59 +0100

    Implement user profile and delete modal

--- ui/assets/js/script.js
+++ ui/assets/js/script.js
@@ -16,3 +16,22 @@ window.onclick = (event) => {
 }
 
 document.querySelector('.mobileMenu').addEventListener("click", slideMobileMenu);
+
+/* MODAL SCRIPT */
+const modal = document.querySelector(".modal");
+    const trigger = document.querySelector(".trigger");
+    const closeButton = document.querySelector(".close-button");
+
+  	const toggleModal = () => {
+        modal.classList.toggle("show-modal");
+    }
+
+    const windowOnClick = (newevent) => {
+        if (newevent.target === modal) {
+            toggleModal();
+        }
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
