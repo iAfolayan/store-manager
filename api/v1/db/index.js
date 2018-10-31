fce698bd328b679fbda69402d46bc2f2c6bcbdbb
Afolayan Isaiah <iafolayanibikunle@gmail.com>
2018-10-31 22:38:17 +0100

    Fetch single sale record

--- api/v1/db/index.js
+++ api/v1/db/index.js
@@ -5,13 +5,20 @@ dotenv.config();
 
 const database = process.env.NODE_ENV === 'test' ? process.env.POSTGRES_DB_TEST : process.env.POSTGRES_DB;
 
-const client = new Pool({
-  host: process.env.POSTGRES_SERVER,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database
-});
+let credential = null;
+if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'production') {
+  credential = process.env.DATABASE_URL;
+} else {
+  credential = {
+    host: process.env.POSTGRES_SERVER,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database
+  };
+}
+
+const client = new Pool(credential);
 
 // Connect DB
 client.connect((err) => {
