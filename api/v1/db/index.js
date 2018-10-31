497461a38ce84668519f92c192d4d0fd3b63c3b8
Afolayan Isaiah <iafolayanibikunle@gmail.com>
2018-11-01 00:24:01 +0100

    Heroku configuration

--- api/v1/db/index.js
+++ api/v1/db/index.js
@@ -5,25 +5,17 @@ dotenv.config();
 
 const database = process.env.NODE_ENV === 'test' ? process.env.POSTGRES_DB_TEST : process.env.POSTGRES_DB;
 
-let credential = null;
-console.log('>>>>>>', process.env.NODE_ENV, '>>>>>>');
-if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'production') {
-  credential = process.env.DATABASE_URL;
-} else {
-  credential = {
-    host: process.env.POSTGRES_SERVER,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database
-  };
-}
-console.log(credential);
-const client = new Pool(credential);
+const client = new Pool({
+  host: process.env.POSTGRES_SERVER,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database
+});
 
 // Connect DB
 client.connect((err) => {
-  if (err) return console.log(err);
+  if (err) return console.log('err', err);
 });
 
 export default client;
